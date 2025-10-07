7b8a8902191c599356c0f28b0a5a08e452d260d9
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-10-07 13:43:59 +0200

    Calculadora final

--- app.py
+++ app.py
@@ -1,13 +1,70 @@
-from ErrorPersonalizado import WithdrawError
+## Calculadora
+from pickletools import optimize
 
 
-def withdraw(amount):
-    if amount < 0:
-        raise WithdrawError(amount)
-    print(f"Has retirado ${amount}")
+# funcion suma
+def add(num1, num2):
+    return num1 + num2
 
 
-try:
-    withdraw(-50)
-except WithdrawError as e:
-    print(f"Error: {e.message} (monto: {e.amount})")
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+
+def division(num1, num2):
+    try:
+        if (num2 == 0):
+            raise ValueError("No se puede dividir entre cero")
+        return num1 / num2
+    except ValueError as e:
+        print(f"Error {e}")
+
+
+def menu():
+    print(f"Seleccione una opcion:")
+    print(f"1-Suma:")
+    print(f"2-Resta:")
+    print(f"3-Multiplicacion:")
+    print(f"4-Division:")
+    print(f"5-Salir:")
+    option = int(input("Dame una opcion: "))
+    return option
+
+
+def operaciones(option):
+    try:
+        if option == 5:
+            print("Saliendo de la calculadora...")
+            return False  # ← salimos del bucle principal
+        else:
+            print("Necesito dos números")
+            number1 = float(input("Dame el primer número: "))
+            number2 = float(input("Dame el segundo número: "))
+
+            if option == 1:
+                print(f"La suma es: {add(number1, number2)}")
+            elif option == 2:
+                print(f"La resta es: {subtract(number1, number2)}")
+            elif option == 3:
+                print(f"La multiplicación es: {multiplicacion(number1, number2)}")
+            elif option == 4:
+                print(f"La división es: {division(number1, number2)}")
+            else:
+                print("La opción no es válida")
+        return True  # continuar el programa
+    except ValueError as e:
+        print(f"Error: entrada no válida {e}")
+    except ZeroDivisionError as e:
+        print("Error: no se puede dividir por cero {e}")
+    return True
+
+
+# Bucle Principal
+while True:
+    opcion = menu()
+    if not operaciones(opcion):
+        break
